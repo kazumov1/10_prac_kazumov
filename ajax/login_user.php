3bb3b07192c0c4ed12c62a7810e883125cd96ffb
Petrovsky <petrovskij8819@mail.ru>
2025-03-10 11:49:37 +0500

    изменили файл со стороны сервера login_user.php

--- ajax/login_user.php
+++ ajax/login_user.php
@@ -1,20 +1,35 @@
 <?php
-	session_start();
-	include("../settings/connect_datebase.php");
-	
-	$login = $_POST['login'];
-	$password = $_POST['password'];
-	
-	// ищем пользователя
-	$query_user = $mysqli->query("SELECT * FROM `users` WHERE `login`='".$login."' AND `password`= '".$password."';");
-	
-	$id = -1;
-	while($user_read = $query_user->fetch_row()) {
-		$id = $user_read[0];
-	}
-	
-	if($id != -1) {
-		$_SESSION['user'] = $id;
-	}
-	echo md5(md5($id));
+session_start();
+include("../settings/connect_datebase.php");
+include("../recaptcha/autoload.php");
+
+$secret = '6LfELu8qAAAAAFU8fmFibldBqJdVbL2pWluqSjCj';
+
+$login = $_POST['login'];
+$password = $_POST['password'];
+
+if (isset($_POST['g-recaptcha-response'])) {
+
+	$recaptcha = new \ReCaptcha\ReCaptcha($secret);
+	$resp = $recaptcha->verify($_POST['g-recaptcha-response'], $_SERVER['REMOTE_ADDR']);
+
+	if ($resp->isSuccess()) {
+		$query_user = $mysqli->query("SELECT * FROM users WHERE login='" . $login . "' AND password= '" . $password . "';");
+		$id = -1;
+		while ($user_read = $query_user->fetch_row()) {
+			$id = $user_read[0];
+		}
+
+		if ($id != -1) {
+			$_SESSION['user'] = $id;
+			echo md5(md5($id));
+		}else{
+			echo "";
+		}
+		
+	} else
+		echo "Пользователь не распознан";
+
+} else
+	echo "Нет ответа от RECAPTURE";
 ?>
